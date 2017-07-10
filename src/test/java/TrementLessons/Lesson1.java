594ec7ae17d8b77ff36e67e46780ff76f4f33890
catal <catalin.geani@live.co.uk>
2017-07-10 12:47:30 +0300

    tremend lesson1

--- src/test/java/TrementLessons/Lesson1.java
+++ src/test/java/TrementLessons/Lesson1.java
@@ -1,8 +1,22 @@
 package TrementLessons;
 
+
 /**
  * Created by catal on 10-Jul-17.
  * Tremend Lesson 1
  */
 public class Lesson1 {
+
+
+    public static void main(String[] args)
+
+    {
+
+/*metoda println()tipăreşte întregul număr la ieşire şi trece în rând nou*/
+
+        System.out.println(2 + 3);
+    }
 }
+
+
+
