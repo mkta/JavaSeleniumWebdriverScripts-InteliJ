3ee656ea60b2647bcb53b9075908da640439fd17
catal <catalin.geani@live.co.uk>
2017-07-10 13:37:51 +0300

    fixed run

--- src/test/PjInteractive_ScriptsBackup/Execute_Desi_FE_LOOP_SCRIPT.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Users\catalinf\Projects\IntJ\DesiLaColScripts
-cd %projectLocation%
-set classpath=%projectLocation%\Req\*;
-java org.testng.TestNG %projectLocation%\testng.xml
-pause
